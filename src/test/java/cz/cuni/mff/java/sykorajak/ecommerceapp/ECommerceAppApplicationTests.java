a2aeca05ded0e9e7ef20104fe53bcc21d897642a
Jakub Sýkora <sykorajakku@jakubs-macbook-pro.home>
2023-07-09 11:12:43 +0200

    Add Spring Initializr generated project files

---
+++ src/test/java/cz/cuni/mff/java/sykorajak/ecommerceapp/ECommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cuni.mff.java.sykorajak.ecommerceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
